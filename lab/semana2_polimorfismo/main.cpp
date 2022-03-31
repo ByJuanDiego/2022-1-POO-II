51b2463e82a2f7ea460170b83f53fc5844184cd4
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-03-31 18:58:25 -0500

    a

---
+++ lab/semana2_polimorfismo/main.cpp
@@ -0,0 +1,29 @@
+#include "Elipse.h"
+#include "Rectangulo.h"
+#include <vector>
+
+/* Requisitos del polimorfismo por herencia
+ 1. Herencia                             -> Definicion de clase
+ 2. Virtualizacion / Sobreescritura      -> Definicion de la clase
+ 3. Uso de punteros                      -> Instanciacion de objetos
+ */
+
+int main() {
+
+    /* Utilidades del polimorfismo
+     - Generar coleccionnes polimorficas
+     - Generar funciones polimorficas
+     */
+
+    vector<Figura*> vector_figuras;
+    vector_figuras.push_back(new Rectangulo);
+    vector_figuras.push_back(new Elipse);
+    vector_figuras.push_back(new Rectangulo);
+    vector_figuras.push_back(new Elipse);
+
+    for (const auto &figura:vector_figuras){
+        figura->dibujar();
+    }
+
+    return 0;
+}
