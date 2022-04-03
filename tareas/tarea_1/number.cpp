a395f0541cf7f0581d146a688914ac5679b8e644
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-04-02 23:45:04 -0500

    Update number.cpp

--- tareas/tarea_1/number.cpp
+++ tareas/tarea_1/number.cpp
@@ -4,27 +4,27 @@
 
 #include "number.h"
 
-integer_t::integer_t(): integer(0){}
+integer_t::integer_t(): number_t(), integer(0){}
 
-integer_t::integer_t(int integer): integer(integer){}
+integer_t::integer_t(int integer): number_t(), integer(integer){}
 
 void integer_t::print(ostream& os) {
     os << integer;
 }
 
 
-real_t::real_t(): real(0){}
+real_t::real_t(): number_t(), real(0){}
 
-real_t::real_t(double real): real(real){}
+real_t::real_t(double real): number_t(), real(real){}
 
 void real_t::print(ostream& os) {
     os << fixed << setprecision(2) << real;
 }
 
 
-complex_t::complex_t(): real(0), imaginario(0) {}
+complex_t::complex_t(): number_t(), real(0), imaginario(0) {}
 
-complex_t::complex_t(double real, double imaginario): real(real), imaginario(imaginario) {}
+complex_t::complex_t(double real, double imaginario): number_t(), real(real), imaginario(imaginario) {}
 
 void complex_t::print(ostream& os) {
     os << fixed << setprecision(2) << real << ((imaginario < 0)? "-": "+" ) << abs(imaginario) << "i";
