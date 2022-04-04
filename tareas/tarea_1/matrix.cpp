33ab711b3365cc4df57b36f6bf15e301c0d5c994
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-04-03 19:03:20 -0500

    Update matrix.cpp

--- tareas/tarea_1/matrix.cpp
+++ tareas/tarea_1/matrix.cpp
@@ -5,26 +5,23 @@
 #include "matrix.h"
 
 namespace utec{
-
     void matrix_t::_assign_(SIZE_TYPE row, SIZE_TYPE col){
         nrow = row;
         ncol = col;
-        data = new number_t*[nrow * ncol];
+        data = new number_t*[nrow * ncol]();
     }
 
     void matrix_t::_copy_(number_t** other_data) {
-        for (int i=0; i<(nrow*ncol); i++) {
+        for (int i=0; i<(nrow*ncol); i++)
             *data[i] = *other_data[i];
-        }
     }
 
     void matrix_t::_destroy_() {
-        for (int i=0; i<(nrow*ncol); i++){
+        for (int i=0; i<(nrow*ncol); i++)
             delete data[i];
-        }
         delete [] data;
     }
-    
+
     ostream& operator<<(ostream &os, const matrix_t &m){
         for (int f = 0; f < m.nrow; f++){
             for (int c = 0; c < m.ncol; c++){
@@ -57,10 +54,9 @@ namespace utec{
     }
 
     matrix_t& matrix_t::operator=(const matrix_t &other){// sobrecarga asignacion copia
-        if (&other == this){
+        if (&other == this)
             return *this;
-        }
-
+        
         _destroy_();
         _assign_(other.nrow, other.ncol);
         _copy_(other.data);
@@ -78,5 +74,4 @@ namespace utec{
     number_t* matrix_t::operator()(SIZE_TYPE i, SIZE_TYPE j) const {
         return data[i * ncol + j];
     }
-
 }
