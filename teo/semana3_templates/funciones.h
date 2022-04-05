f5dc083af752f150f04dbb7f427ab6542b91f966
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-04-05 16:03:58 -0500

    Update funciones.h

--- teo/semana3_templates/funciones.h
+++ teo/semana3_templates/funciones.h
@@ -5,10 +5,12 @@
 #ifndef SEMANA3_TEMPLATES_FUNCIONES_H
 #define SEMANA3_TEMPLATES_FUNCIONES_H
 
-template <int N, int P> struct Power{
+template <int N, int P>
+struct Power{
     enum {val = N * Power<N, P-1>::val};
 };
-template <int N> struct Power<N, 0>{
+template <int N>
+struct Power<N, 0>{
     enum {val = 1};
 };
 
