07ebbdf854e4c45b1d86d9f82997f50b93661070
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-04-08 22:16:23 -0500

    a

--- lab/semana3_templates_1/main.cpp
+++ lab/semana3_templates_1/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cstring>
 #include <array>
-#include <vector>
-#include <list>
 using namespace std;
 
 /*
@@ -101,30 +99,6 @@ void f(){
     cout << "a: " << a << " b: " << b << endl;
 }
 
-template <typename T, int sz>
-auto get_size(const T (&)[sz]){// obtener el tamaño de un array estatico
-    return sz;
-}
-
-template<typename T, size_t sz>
-int get_size(array<T, sz>){// obtener el tamaño de un array
-    return sz;
-}
-
-template <template<typename...> class Container, typename T>
-void push(Container<T>& contenedor, T value){// obtener el tamaño de un array
-    contenedor.push_back(value);
-}
-
-template<int N>
-constexpr int factorial(){
-    return N * factorial<N-1>();
-}
-template<>
-constexpr int factorial<0>(){
-    return 1;
-}
-
 int main() {
 
     show("Juan");
@@ -150,32 +124,6 @@ int main() {
     f(a, b);    // funciona
     f<a, b>();  // no funciona, (a) y (b) no existen en tiempo de compilacion.
     */
-
-    int arr1[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::array<int, 2> arr2 {0, 1};
-
-    cout << "arr[10] = " << get_size(arr1) << endl;
-    cout << "array<int, 2> arr2 = " << get_size(arr2) << endl;
-
-    std::vector<int> vec;
-    vec.push_back(1);
-    push<vector>(vec, 2);
-    push<vector>(vec, 3);
-    for (const auto &i: vec){
-        cout << i << " ";
-    }
-    cout << endl;
-
-    std::list<double> lst;
-    lst.push_back(1.1);
-    push<list>(lst, 2.2);
-    push<list>(lst, 3.3);
-    for (const auto &i: lst){
-        cout << i << " ";
-    }
-    cout << endl;
-
-    cout << "5! = " << factorial<5>() << endl;
 
     return 0;
 }
---
+++ lab/semana3_templates_2/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <list>
+using namespace std;
+
+template <typename T, int sz>
+auto get_size(const T (&)[sz]){// obtener el tamaño de un array estatico
+    return sz;
+}
+
+template<typename T, size_t sz>
+int get_size(std::array<T, sz>){// obtener el tamaño de un std::array
+    return sz;
+}
+
+template <template<typename...> class Container, typename T>
+void push(Container<T>& contenedor, T value){// obtener el tamaño de un array
+    contenedor.push_back(value);
+}
+
+template<int N>
+constexpr int factorial(){
+    return N * factorial<N-1>();
+}
+template<>
+constexpr int factorial<0>(){
+    return 1;
+}
+
+int main() {
+
+    int arr1[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::array<int, 2> arr2 {0, 1};
+
+    cout << "arr[10] = " << get_size(arr1) << endl;
+    cout << "array<int, 2> arr2 = " << get_size(arr2) << endl;
+
+    std::vector<int> vec;
+    vec.push_back(1);
+    push<vector>(vec, 2);
+    push<vector>(vec, 3);
+    for (const auto &i: vec){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    std::list<double> lst;
+    lst.push_back(1.1);
+    push<list>(lst, 2.2);
+    push<list>(lst, 3.3);
+    for (const auto &i: lst){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout << "5! = " << factorial<5>() << endl;
+
+    return 0;
+}
