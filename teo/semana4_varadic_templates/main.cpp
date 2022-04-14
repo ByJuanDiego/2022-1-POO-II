ff854194da65ae7697fb9aecfa009147d7c3ebf3
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-04-14 13:49:54 -0500

    a

--- teo/semana4_varadic_templates/main.cpp
+++ teo/semana4_varadic_templates/main.cpp
@@ -87,11 +87,11 @@ auto suma2(Params... params){
 }
 
 template<typename T>
-constexpr bool is_int(){
+constexpr bool is_int(T param){
     return false;
 }
 template<>
-constexpr bool is_int<int>(){
+constexpr bool is_int<int>(int param){
     return true;
 }
 
@@ -107,12 +107,12 @@ constexpr bool is_vector<vector>(){
 template<
         template <typename...> class First,
         template <typename...> class... ParamTemplatePackage>
-constexpr void f11() {
+constexpr void f7() {
     // Verifica si los params son del template vector
     cout << "is_vector()?: ";
     cout << boolalpha << is_vector<First>() << endl;
     if constexpr (sizeof...(ParamTemplatePackage) > 0)
-        f11<ParamTemplatePackage...>();
+        f7<ParamTemplatePackage...>();
 }
 
 template <typename... ParamPackage>
@@ -153,21 +153,21 @@ int main() {
     f4(1, -3, 4.3, 4.5, string("Hola"));
     f5<1, 2, 3, 4, 5>();
     f6<vector, map, list, basic_string>();
+    f7<vector, map, vector, list>();
 
     print1(1, 2, 4.5, string("A"));
     print2(0, -1, 3.14, string("B"));
 
-    cout << "is_int(int): " << boolalpha << (is_int<int>()) << endl;
-    cout << "is_int(bool): " << boolalpha << (is_int<bool>()) << endl;
-
-    f11<vector, map, vector, list>();
-
+    cout << "is_int(10): " << boolalpha << (is_int(10)) << endl;
+    cout << "is_int(1.0): " << boolalpha << (is_int(1.0)) << endl;
+    cout << "is_int('A'): " << boolalpha << (is_int("A")) << endl;
+    
     cout << "len(1,2,3) = " << size(1, 2, 3) << endl;
     cout << "min(9,1,2) = " << menor(9, 1, 2) << endl;
     cout << "max(7,9,3) = " << mayor(7, 9, 3) << endl;
     cout << "sum1(1+2+3) = " << suma1(1, 2, 3) << endl;
     cout << "sum2(4+5+6) = " << suma2(4, 5, 6) << endl;
     cout << "mean(1,9,3) = " << media(1, 9, 3) << endl;
-    
+
     return 0;
 }
