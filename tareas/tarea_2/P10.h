56c921b92fa949ffcce59a74a95eb0811a8a7a01
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-04-19 07:27:20 -0500

    a

--- tareas/tarea_2/P10.h
+++ tareas/tarea_2/P10.h
@@ -8,85 +8,65 @@
 #include <array>
 using namespace std;
 
+template<typename cont>
+int linearSearch(int value, cont*& container){
+    for (int i=0; i<container->size(); i++){
+        if ((*container)[i] == value){
+            return i;
+        }
+    }
+    return -1;
+}
+
+template<typename cont>
+void bubbleSort(cont*& container){
+    for (int i = 0; i < container->size() - 1; i++) {
+        for (int j = 0; j < container->size() - i - 1; j++) {
+            if ((*container)[j] > (*container)[j + 1]) {
+                typename cont::value_type temp = (*container)[j];
+                (*container)[j] = (*container)[j+1];
+                (*container)[j+1] = temp;
+            }
+        }
+    }
+}
+
 template<typename Container>
 class searcher{
+
+    template<typename cont>
+    friend int linearSearch(int value, cont*& container);
+
+    template<typename cont>
+    friend void bubbleSort(cont*& container);
+
 private:
     Container* container;
-    void bubbleSort(){
-        for (int i = 0; i < container->size() - 1; i++) {
-            for (int j = 0; j < container->size() - i - 1; j++) {
-                if ((*container)[j] > (*container)[j + 1]) {
-                    typename Container::value_type temp = (*container)[j];
-                    (*container)[j] = (*container)[j+1];
-                    (*container)[j+1] = temp;
-                }
-            }
-        }
-    }
-    int linearSearch(int value){
-        for (int i=0; i<container->size(); i++){
-            if ((*container)[i] == value){
-                return i;
-            }
-        }
-        return -1;
-    }
-
 public:
     searcher(Container& other){
         container = &other;
-        bubbleSort();
+        bubbleSort(container);
     }
     int operator << (int x){
-        return linearSearch(x);
+        return linearSearch(x, container);
     }
 };
+
+
 
 template<typename T, size_t sz>
 class searcher<array<T, sz>>{
 private:
     array<T, sz>* container;
-    void bubbleSort(){
-        for (int i = 0; i < sz - 1; i++) {
-            for (int j = 0; j < sz - i - 1; j++) {
-                if ((*container)[j] > (*container)[j + 1]) {
-                    T temp = (*container)[j];
-                    (*container)[j] = (*container)[j+1];
-                    (*container)[j+1] = temp;
-                }
-            }
-        }
-    }
-    int linearSearch(int value){
-        for (int i=0; i<container->size(); i++){
-            if ((*container)[i] == value){
-                return i;
-            }
-        }
-        return -1;
-    }
 public:
     searcher(array<T, sz>& other){
         container = &other;
-        bubbleSort();
+        bubbleSort(container);
     }
     int operator << (int x){
-        return linearSearch(x);
+        return linearSearch(x, container);
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #endif //PROG3_UNIT2_TEMPLATES_V2022_1_P10_H
