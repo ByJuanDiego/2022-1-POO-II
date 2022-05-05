67d5f7c4cac81579d35a50737a9ec64f80d66625
ByJuanDiego <79115974+ByJuanDiego@users.noreply.github.com>
2022-05-05 09:45:02 -0500

    a

--- lab/s6_algoritmos/cmake-build-debug/cmake_install.cmake
+++ lab/s6_algoritmos/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/juandiegocastropadilla/Desktop/CS1103/lab/s6_algoritmos
+# Install script for directory: /Users/juandiegocastropadilla/Desktop/CS2013/lab/s6_algoritmos
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/juandiegocastropadilla/Desktop/CS1103/lab/s6_algoritmos/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/juandiegocastropadilla/Desktop/CS2013/lab/s6_algoritmos/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- lab/s6_algoritmos/main.cpp
+++ lab/s6_algoritmos/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <iomanip>
 #include <functional>
 
 #define PATRONES
